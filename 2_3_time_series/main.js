33931bb40b926e8537d458dcc25fb11700da0a35
pdesai97 <pdesai@gradcenter.cuny.edu>
2022-10-29 22:32:20 -0400

    assignment

--- 2_3_time_series/main.js
+++ 2_3_time_series/main.js
@@ -1,26 +1,60 @@
  /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ;
+ const  margin = {top: 30, right: 30, bottom: 30, left: 60}
+ const width = window.innerWidth * .7 - margin.left - margin.right
+ const  height = window.innerHeight * .7 - margin.top - margin.bottom
 
 /* LOAD DATA */
-d3.csv('[PATH_TO_YOUR_DATA]', d => {
+d3.csv('Christmas spending.csv', d => {
   return {
-    // year: new Date(+d.Year, 0, 1),
-    // country: d.Entity,
-    // population: +d.Population
+    date: new Date(+d.Year, 0, 1),
+    spend: +d.Christmas_spend_in_US$ 
   }
-}).then(data => {
-  console.log('data :>> ', data);
-
-  // SCALES
+  }).then(data => {
+    console.log("data", data);
 
   // CREATE SVG ELEMENT
 
-  // BUILD AND CALL AXES
+const svg = d3.select("#container")
+  .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+          "translate(" + margin.left + "," + margin.top + ")"); 
+
+  // SCALES
+
+const  xScale = d3.scaleLinear()
+  .domain( d3.extent(data, d => d.date ))
+  // .domain(d3.extent(data, d => d.Year))
+  .range([ 0, width ]);
+svg.append("g")
+  .attr("transform", "translate(0," + height + ")")
+  .call(d3.axisBottom(xScale));
+
+const yScale = d3.scaleLinear()
+  .domain([0, d3.max(data, d => d.spend)])
+  // .domain(d3.extent(data, d => d.Christmas_spend_in_US$))
+  .range([ height, margin.top ]);
+svg.append("g")
+  .call(d3.axisLeft(yScale));
+
 
   // LINE GENERATOR FUNCTION
 
+const line = d3.line()
+    .x(d => xScale(d.date))
+    .y(d => yScale(d.spend))
+
+
+const path = svg.selectAll("paths")
+    .data(data)
+    .join("path")
+    .attr("stroke", "white")
+    .attr("fill", "cyan")
+    .attr("d", d => line(d))
+
+
   // DRAW LINE
 
-});
+})
